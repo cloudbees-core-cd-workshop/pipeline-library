22119cef3245840eaacabc4c2947d2ead0fb3572
Kurt Madel <kmadel@cloudbees.com>
2018-09-07 08:58:53 -0400

    Create setECRLifecyclePolicy.groovy
    for automatic cleanup of all docker images pushed to ECR

---
+++ var/setECRLifecyclePolicy.groovy
@@ -0,0 +1,21 @@
+// vars/setECRLifecyclePolicy.groovy
+def call(String name, Closure body) {
+  def label = "aws-cli}"
+  def podYaml = libraryResource 'podtemplates/awsCli.yml'
+  podTemplate(name: 'aws-cli', label: label, yaml: podYaml) {
+    node(label) {
+      container('aws-cli') {
+        def lifecyclePolicy = libraryResource 'aws/ecr/lifecycle-policy/tempImagePolicy.json'
+        sh """aws ecr put-lifecycle-policy --region us-east-1 --repository-name ${name} --lifecycle-policy-text '$lifecyclePolicy'"""
+        try {
+          errorMsg = sh(returnStdout: true, script: "aws ecr create-repository --region us-east-1 --repository-name ${name} | tr -d '\n'")
+        } catch(e) {
+          //error other than for RepositoryAlreadyExistsException
+          if(!errorMsg.contains("RepositoryAlreadyExistsException")) {
+            throw e
+          }
+        }
+      }
+    }
+  }
+}
