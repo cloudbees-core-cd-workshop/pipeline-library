b027b327aafafee97d334b50049db20841a29d85
Kurt Madel <kmadel@cloudbees.com>
2018-09-07 09:01:54 -0400

    Update dockerBuildPush.groovy
    added setECRLifecyclePolicy

--- var/dockerBuildPush.groovy
+++ var/dockerBuildPush.groovy
@@ -1,18 +1,19 @@
 // vars/dockerBuildPush.groovy
 def call(String dockerRepo, String tag, String target = ".", String dockerFile="Dockerfile", Closure body) {
-    def dockerReg = "946759952272.dkr.ecr.us-east-1.amazonaws.com"
-    def label = "kaniko"
-    def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
-    podTemplate(name: 'kaniko', label: label, namespace: 'kaniko',  yaml: podYaml) {
-      node(label) {
-        container(name: 'kaniko', shell: '/busybox/sh') {
-          body()
-          withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
-            sh """#!/busybox/sh
-              executor -f ${pwd()}/${dockerFile} -c ${pwd()} -d ${dockerRepo}${name}:${tag} -d ${dockerRepo}${name}:latest
-            """
-          }
+  def dockerReg = "946759952272.dkr.ecr.us-east-1.amazonaws.com"
+  def label = "kaniko"
+  def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
+  podTemplate(name: 'kaniko', label: label, namespace: 'kaniko',  yaml: podYaml) {
+    node(label) {
+      container(name: 'kaniko', shell: '/busybox/sh') {
+        body()
+        withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
+          sh """#!/busybox/sh
+            executor -f ${pwd()}/${dockerFile} -c ${pwd()} -d ${dockerRepo}${name}:${tag} -d ${dockerRepo}${name}:latest
+          """
+        }
         }
       }
     }
+    setECRLifecyclePolicy("${name}")
 }
