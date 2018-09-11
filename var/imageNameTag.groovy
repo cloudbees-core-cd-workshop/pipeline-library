fdd2efbf3b398b18baaccf8390713109726debd7
Kurt Madel <kmadel@cloudbees.com>
2018-09-11 13:24:15 -0400

    Update imageNameTag.groovy
    separated name into name and repo

--- var/imageNameTag.groovy
+++ var/imageNameTag.groovy
@@ -1,6 +1,7 @@
 //imageNameTag.groovy
 def call(body) {
   tokens = "${env.JOB_NAME}".tokenize('/')
-  env.IMAGE_NAME = tokens[tokens.size()-3] + "/" + tokens[tokens.size()-2]
+  env.IMAGE_REPO = tokens[tokens.size()-3]
+  env.IMAGE_NAME = tokens[tokens.size()-2]
   env.IMAGE_TAG = tokens[tokens.size()-1]
 }
