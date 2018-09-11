71db928381c062eb34b89e76d67559ed2ef98a1e
Kurt Madel <kmadel@cloudbees.com>
2018-09-11 13:32:09 -0400

    Update dockerBuildPush.groovy
    Updated to pull image repo from env, added imageName param

--- var/dockerBuildPush.groovy
+++ var/dockerBuildPush.groovy
@@ -1,5 +1,5 @@
 // vars/dockerBuildPush.groovy
-def call(String dockerRepo, String tag = env.BUILD_NUMBER, String target = ".", String dockerFile="Dockerfile", Closure body) {
+def call(String imageName, String imageTage = env.BUILD_NUMBER, String target = ".", String dockerFile="Dockerfile", Closure body) {
   def dockerReg = "946759952272.dkr.ecr.us-east-1.amazonaws.com"
   setECRLifecyclePolicy("${dockerRepo}")
   def label = "kaniko"
@@ -10,7 +10,7 @@ def call(String dockerRepo, String tag = env.BUILD_NUMBER, String target = ".", 
         body()
         withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
           sh """#!/busybox/sh
-            executor -f ${pwd()}/${dockerFile} -c ${pwd()} --build-arg buildNumber=${BUILD_NUMBER} -d ${dockerReg}/${dockerRepo}:${tag}
+            executor -f ${pwd()}/${dockerFile} -c ${pwd()} --build-arg buildNumber=${BUILD_NUMBER} -d ${dockerReg}/${env.IMAGE_REPO}/${imageNamae}:${imageTage}
           """
         }
         }
