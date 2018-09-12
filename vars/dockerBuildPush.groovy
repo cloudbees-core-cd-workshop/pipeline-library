59108bbe7bd0c6bc0e09ef0cfef39398a8dedf1f
Kurt Madel <kmadel@cloudbees.com>
2018-09-12 15:26:18 -0400

    Update dockerBuildPush.groovy
    Fix for setECRLifecyclePolicy call

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,7 +1,8 @@
 // vars/dockerBuildPush.groovy
 def call(String imageName, String imageTag = env.BUILD_NUMBER, String target = ".", String dockerFile="Dockerfile", Closure body) {
   def dockerReg = "946759952272.dkr.ecr.us-east-1.amazonaws.com"
-  setECRLifecyclePolicy("${env.IMAGE_REPO}/${imageName}")
+  def repoName = "${env.IMAGE_REPO}/${imageName}"
+  setECRLifecyclePolicy(repoName)
   def label = "kaniko"
   def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
   podTemplate(name: 'kaniko', label: label, namespace: 'kaniko',  yaml: podYaml) {
@@ -10,7 +11,7 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String target = "
         body()
         withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
           sh """#!/busybox/sh
-            executor -f ${pwd()}/${dockerFile} -c ${pwd()} --build-arg buildNumber=${BUILD_NUMBER} -d ${dockerReg}/${env.IMAGE_REPO}/${imageName}:${imageTag}
+            executor -f ${pwd()}/${dockerFile} -c ${pwd()} --build-arg buildNumber=${BUILD_NUMBER} -d ${dockerReg}/${repoName}:${imageTag}
           """
         }
         }
