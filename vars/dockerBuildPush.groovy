8586f589197945e5d6c336370db59576199272af
Kurt Madel <kmadel@cloudbees.com>
2018-09-12 15:38:06 -0400

    Update dockerBuildPush.groovy
    Removed stray }

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,7 +1,7 @@
 // vars/dockerBuildPush.groovy
 def call(String imageName, String imageTag = env.BUILD_NUMBER, String target = ".", String dockerFile="Dockerfile", Closure body) {
   def dockerReg = "946759952272.dkr.ecr.us-east-1.amazonaws.com"
-  def repoName = env.IMAGE_REPO} + "/" + imageName
+  def repoName = env.IMAGE_REPO + "/" + imageName
   setECRLifecyclePolicy(repoName)
   def label = "kaniko"
   def podYaml = libraryResource 'podtemplates/dockerBuildPush.yml'
