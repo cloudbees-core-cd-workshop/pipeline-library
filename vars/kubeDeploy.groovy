b0f5ab6e712bb0fa93308c57757094fcfd74bef8
Kurt Madel <kmadel@cloudbees.com>
2018-09-12 16:12:51 -0400

    Update kubeDeploy.groovy
    move writeFile into node

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -3,9 +3,9 @@ def call(imageRepo, imageName, imageTag) {
     def label = "kubectl"
     def podYaml = libraryResource 'podtemplates/kubeDeploy.yml'
     def deployYaml = libraryResource 'k8s/basicDeploy.yml'
-    writeFile file: "deploy.yml", text: deployYaml
     podTemplate(name: 'kubectl', label: label, namespace: 'cb-deploy',  yaml: podYaml) {
       node(label) {
+        writeFile file: "deploy.yml", text: deployYaml
         container("kubectl") {
           sh("sed -i.bak 's#REPLACE_IMAGE_TAG#946759952272.dkr.ecr.us-east-1.amazonaws.com/${imageRepo}/${imageName}:${imageTag}#' deploy.yml")
           sh("sed -i.bak 's#REPLACE_SERVICE_NAME#${imageRepo}-${imageName}#' deploy.yml")
