c8bad815b75b53acf4ce7e64e66579ec04fe49b4
Kurt Madel <kmadel@cloudbees.com>
2018-09-12 15:48:13 -0400

    Update setECRLifecyclePolicy.groovy
    moved put-lifecycle-policy

--- vars/setECRLifecyclePolicy.groovy
+++ vars/setECRLifecyclePolicy.groovy
@@ -5,10 +5,10 @@ def call(String repoName) {
   podTemplate(name: 'aws-cli', label: label, yaml: podYaml) {
     node(label) {
       container('aws-cli') {
-        def lifecyclePolicy = libraryResource 'aws/ecr/lifecycle-policy/tempImagePolicy.json'
-        sh """aws ecr put-lifecycle-policy --region us-east-1 --repository-name ${repoName} --lifecycle-policy-text '$lifecyclePolicy'"""
         try {
           errorMsg = sh(returnStdout: true, script: "aws ecr create-repository --region us-east-1 --repository-name ${repoName} | tr -d '\n'")
+          def lifecyclePolicy = libraryResource 'aws/ecr/lifecycle-policy/tempImagePolicy.json'
+          sh """aws ecr put-lifecycle-policy --region us-east-1 --repository-name ${repoName} --lifecycle-policy-text '$lifecyclePolicy'"""
         } catch(e) {
           //error other than for RepositoryAlreadyExistsException
           if(!errorMsg.contains("RepositoryAlreadyExistsException")) {
