a90b850d8a88979ba37e4d5d1b6001fd4f89ecb6
Kurt Madel <kmadel@cloudbees.com>
2018-09-13 08:40:24 -0400

    Update and rename gitShortCommit.groovy to gitShortCommitAuthor.groovy
    adde author

--- vars/gitShortCommitAuthor.groovy
+++ vars/gitShortCommitAuthor.groovy
@@ -2,6 +2,10 @@
 def call(size) {
     size = size ?: 7
     def SHORT_COMMIT = sh(returnStdout: true, script: "git rev-parse HEAD | cut -c1-${size} | tr -d '\n'")
-    sh "echo $SHORT_COMMIT"
+    def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
+    def author = sh(returnStdout: true, script: "git --no-pager show -s --format='%an' ${commit} | tr -d '\n'")
+    sh "echo ${SHORT_COMMIT}"
+    sh "echo ${author}"
     env.SHORT_COMMIT = SHORT_COMMIT
+    env.COMMIT_AUTHOR = author
 }
