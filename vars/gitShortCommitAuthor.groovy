4b8875c0959daaca329b1ca810ca4b9514b38893
Kurt Madel <kmadel@cloudbees.com>
2018-09-13 08:49:03 -0400

    Update gitShortCommitAuthor.groovy
    fix for getting author

--- vars/gitShortCommitAuthor.groovy
+++ vars/gitShortCommitAuthor.groovy
@@ -1,11 +1,10 @@
 //will set an environmental variable to short commit, must of git repo in workspace
 def call(size) {
     size = size ?: 7
-    def SHORT_COMMIT = sh(returnStdout: true, script: "git rev-parse HEAD | cut -c1-${size} | tr -d '\n'")
-    def commit = sh(returnStdout: true, script: 'git rev-parse HEAD')
-    def author = sh(returnStdout: true, script: "git --no-pager show -s --format='%an' ${commit} | tr -d '\n'")
-    sh "echo ${SHORT_COMMIT}"
+    def shortCommit = sh(returnStdout: true, script: "git rev-parse HEAD | cut -c1-${size} | tr -d '\n'")
+    def author = sh(returnStdout: true, script: "git --no-pager show -s --format='%an' ${SHORT_COMMIT} | tr -d '\n'")
+    sh "echo ${shortCommit}"
     sh "echo ${author}"
-    env.SHORT_COMMIT = SHORT_COMMIT
+    env.SHORT_COMMIT = shortCommit
     env.COMMIT_AUTHOR = author
 }
