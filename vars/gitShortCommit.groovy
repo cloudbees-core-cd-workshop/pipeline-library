5d707b476cffa9136a741d70e519b68b6011863f
Kurt Madel <kmadel@cloudbees.com>
2018-09-16 16:36:12 -0400

    Update gitShortCommit.groovy

--- vars/gitShortCommit.groovy
+++ vars/gitShortCommit.groovy
@@ -2,5 +2,7 @@
 def call(size) {
     size = size ?: 7
     env.SHORT_COMMIT = sh(returnStdout: true, script: "git rev-parse HEAD | cut -c1-${size} | tr -d '\n'")
+    env.COMMIT_AUTHOR = sh(returnStdout: true, script: "git --no-pager show -s --format='%an' ${SHORT_COMMIT} | tr -d '\n'")
     sh "echo ${SHORT_COMMIT}"
+    sh "echo ${COMMIT_AUTHOR}"
 }
