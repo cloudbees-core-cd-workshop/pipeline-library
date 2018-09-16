a4f47010f1c70dce9b76b7c07450f29eed389264
Kurt Madel <kmadel@cloudbees.com>
2018-09-16 16:46:43 -0400

    Update defineProps.groovy

--- vars/defineProps.groovy
+++ vars/defineProps.groovy
@@ -1,5 +1,7 @@
 // vars/defineProps.groovy
 def call(String file, Map defaults) {
+  //defaults from job name for Docker image
+  imageNameTag()
   //set SHORT_COMMIT for all defineProps calls
   gitShortCommit()
   //use the Pipeline Utility Steps plugin readProperties step to read the .nodejs-app custom marker file 
