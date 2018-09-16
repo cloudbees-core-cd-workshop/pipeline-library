96c21c74e65f3d3236bdb6c74bc51f18c1003210
Kurt Madel <kmadel@cloudbees.com>
2018-09-16 16:31:02 -0400

    Update defineProps.groovy

--- vars/defineProps.groovy
+++ vars/defineProps.groovy
@@ -1,5 +1,7 @@
 // vars/defineProps.groovy
 def call(String file, Map defaults) {
+  //set SHORT_COMMIT for all defineProps calls
+  gitShortCommit()
   //use the Pipeline Utility Steps plugin readProperties step to read the .nodejs-app custom marker file 
   def props = readProperties defaults: defaults, file: file
   for ( e in props ) {
