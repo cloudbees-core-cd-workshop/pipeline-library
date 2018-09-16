314ac2542ace16445a7df91a74964268c18c41cc
Kurt Madel <kmadel@cloudbees.com>
2018-09-16 16:14:07 -0400

    Update and rename gitShortCommitAuthor.groovy to gitShortCommit.groovy
    removed author

---
+++ vars/gitShortCommit.groovy
@@ -0,0 +1,6 @@
+//will set an environmental variable to short commit, must of git repo in workspace
+def call(size) {
+    size = size ?: 7
+    env.SHORT_COMMIT = sh(returnStdout: true, script: "git rev-parse HEAD | cut -c1-${size} | tr -d '\n'")
+    sh "echo ${SHORT_COMMIT}"
+}
