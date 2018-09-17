56390c9ad7a1c77ee677836bff063ccb00e78b25
Kurt Madel <kmadel@cloudbees.com>
2018-09-16 22:40:50 -0700

    Update setECRLifecyclePolicy.groovy
    ECR repository-name has to be lowercase

--- vars/setECRLifecyclePolicy.groovy
+++ vars/setECRLifecyclePolicy.groovy
@@ -1,5 +1,6 @@
 // vars/setECRLifecyclePolicy.groovy
 def call(String repoName) {
+  repoName = repoName.toLowerCase()
   def label = "aws-cli"
   def podYaml = libraryResource 'podtemplates/awsCli.yml'
   podTemplate(name: 'aws-cli', label: label, yaml: podYaml) {
